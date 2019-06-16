37f59e4b8432d9cd371e971477d2a7ca38237f1a
guilherme2020 <guilhermerodrigues123477@gmail.com>
2019-06-16 15:15:13 -0300

    configuracao do formulario, feed, like e reatime com socket.io-client

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,6 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import io from 'socket.io-client'
 
+import api from '../services/api'
 import './Feed.css'
+
 import more from '../assets/more.svg'
 import like from '../assets/like.svg'
 import comment from '../assets/comment.svg'
@@ -8,40 +11,95 @@ import send from '../assets/send.svg'
 
 class Feed extends Component {
 
-    render(){
-        return(
-            <section id='post-list'> 
-                <article>
-                    <header>
-                        <div className='user-info'>
-                            <span>
-                                Diego Fernandes
-                            </span>
-                            <span className='place'>
-                                Rio do sul
-                            </span>
+    state = {
+        feed: []
+    }
 
-                        </div>
-                        <img src={more} alt="Mais" />
+    registerToSocket = () => {
+
+        let urlBase = 'http://localhost:3333';
+        let socket = io(urlBase);
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] })
+        })
+        socket.on('like',likedPost => {
+            let liked = this.state.feed.map(post => post._id === likedPost._id ? likedPost : post)
+            this.setState({
+                feed: liked
+            })
+        })
+    }
+
+    async componentDidMount() {
+        this.registerToSocket()
+
+
+        let url = 'posts'
+        const response = await api.get(url);
+
+        this.setState({
+            feed: response.data
+        })
+
+
+    }
+
+    handleLike = id => {
+        let url = `/posts/${id}/like`
+        api.post(url)
+    }
+    render() {
+        return (
+            <section id='post-list'>
+                {this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className='user-info'>
+                                <span>
+                                    {post.author}
+                                </span>
+                                <span className='place'>
+                                    {post.place}
+                                </span>
+
+                            </div>
+                            <img src={more} alt="Mais" />
 
 
 
-                    </header>
+                        </header>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="Post" />
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="like" />
-                            <img src={comment} alt="comment" />
-                            <img src={send} alt="send" />
-                        </div>
-                    </footer>
-                </article>
+                        <footer>
+                            <div className="actions">
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+
+                                    <img src={like} alt="like" />
+
+
+                                </button>
+                                <img src={comment} alt="comment" />
+                                <img src={send} alt="send" />
+                            </div>
+
+                            <strong>
+                                {post.likes} curtidas
+                            </strong>
+
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+
+                        </footer>
+                    </article>
+
+                ))}
             </section>
         )
-    }    
+    }
 
 }
 export default Feed;
 
-
-//  <img src="http://localhost:3333/files/"  />/
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,84 @@
 import React, {Component} from 'react';
+import './New.css'
+import api from '../services/api'
 
 class New extends Component {
+    state = {
+        image: null,author:"",place:'',description:'',hashtags: ''
 
+    }
+
+
+    handleChange = e => {
+
+        this.setState({  [e.target.name] : e.target.value  })
+
+    }
+    
+    handleSubmit = async e => {
+        e.preventDefault()
+        console.log(this.state);
+
+        const data = new FormData()
+
+        data.append('image',this.state.image)
+        data.append('author',this.state.author)
+        data.append('place',this.state.place)
+        data.append('description',this.state.description)
+        data.append('hashtags',this.state.hashtags)
+
+
+        await api.post('posts',data)
+
+        this.props.history.push('/')
+    }
+
+    handleImageChange = e => {
+        this.setState({ image: e.target.files[0] })
+    }
     render(){
         return(
-            <h1>New</h1>
+            <form id="new-post" onSubmit={this.handleSubmit}> 
+
+                <input  type="file" onChange={this.handleImageChange} />
+
+                <input 
+                    type="text" 
+                    name="author"
+                    placeholder="Autor do post"
+                    value={this.state.author}
+                    onChange={this.handleChange}
+                />
+
+                <input 
+                    type="text"
+                    name="place"
+                    value={this.state.place}
+                    placeholder="Local do post"
+                    onChange={this.handleChange}
+                />
+
+                <input 
+                    type="text"
+                    name="description"
+                    value={this.state.description}
+                    placeholder="Descricao do post"
+                    onChange={this.handleChange}
+                />
+
+                <input 
+                    type="text"
+                    name="hashtags"
+                    value={this.state.hashtags}
+                    placeholder="Hashtags do post"            
+                    onChange={this.handleChange}
+                />
+                <button type="submit">
+                    
+                    Enviar
+
+                </button>
+            </form>
         )
     }    
 
